ac4ffd01284e9d9927566f6d206bb46640cdeb56
Michallesss <mwiemszra@gmail.com>
2021-09-20 20:39:14 +0200

    Function improvment
    Ogólnie to pewnie jedyne co nam się z tego przyda to pobieranie z pliku (które nawet nie jest gotowe)

--- main.cpp
+++ main.cpp
@@ -12,12 +12,14 @@ string table[1000][1000];
 
 void Create_Database()
 {
-    //Create Database.
+    cin>>databasename;
+    cout<<"CREATE DATABASE"<<databasename<<";"<<endl;
 }
 
 void Drop_Database()
 {
-    //Drop Database.
+    cin>>databasename;
+    cout<<"DROP DATABASE"<<databasename<<";"<<endl;
 }
 
 void Create_Table()
@@ -35,9 +37,73 @@ void Insert_Into()
     //Insert Into.
 }
 
+void Conect()
+{
+    //Conect tabels.
+}
+
+void Get_File()
+{
+    int nr_lini=1;
+
+    fstream plik;
+    plik.open("code.txt", ios::in);
+    if(plik.good()==false)
+    {
+    cout<<"Nie mozna otworzyc pliku!";
+    exit(0);
+    }
+    while (getline(plik, linia))
+    {
+        //todo.
+    }
+    plik.close(); 
+}
+
+
+
+void Question()
+{
+    cin>>command;
+
+    if(command=="database")
+    {
+        Create_Database();
+    }
+    else if(command=="drop database")
+    {
+        Drop_Database();
+    }
+    else if(command=="table")
+    {
+        Create_Table();
+    }
+    else if(command=="drop table")
+    {
+        Drop_Table();
+    }
+    else if(command=="insert")
+    {
+        Insert_Into();
+    }
+    else if(command=="conect")
+    {
+        Conect();
+    }
+    else if(command=="get file")
+    {
+        Get_File();
+    }
+    else
+    {
+        cout<<"Invalid command!";
+        exit(0);
+    }
+}
 
 int main()
 {
+    Question();
 
     return 0;
 }
